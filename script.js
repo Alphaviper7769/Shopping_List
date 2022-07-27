af2bb662c47e13e51c50acfe8e5bc63cd76dac55
Alphaviper7769 <aayushraturi@gmail.com>
2022-07-28 01:24:04 +0530

    -2nd commit

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var le=document.querySelectorAll("li");
 
 function inputLength() {
 	return input.value.length;
 }
+
+
 
 function createListElement() {
 	var li = document.createElement("li");
@@ -25,8 +28,22 @@ function addListAfterKeypress(event) {
 	}
 }
 
+function editList(event,i) {
+	
+	le[i].classList.add("done");
+
+}
+
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
 
+
+for(var i=0;i<le.length;i++)
+{
+	
+	le[i].addEventListener("click", editList(event,i));
+
+}
+ 
 
